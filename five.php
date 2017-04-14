bd6256f7a904e274093b55c3436ddf9b38f75d4d
abheemireddy <abheemireddy@unomaha.edu>
2017-04-13 22:10:02 -0500

    Add files via upload

--- five.php
+++ five.php
@@ -1,27 +1,12 @@
 
 <form action="five.php" method="post">
-<label for="ID">Athlete_id</label>
-<input type="number" name="Athlete_id" id="Athlete_id">
-<br/>
-<label for="ID">Name</label>
-<input type="varchar" name="Name" id="Name">
-<br/>
-<label for="ID">DOB</label>
-<input type="date" name="DOB" id="DOB">
-<br/>
-<label for="ID">Gender</label>
-<input type="varchar" name="Gender" id="Gender">
-<br/>
 <label for="ID">Country</label>
-<input type="varchar" name="Country" id="Country">
+<input type="text" name="Country" id="Country">
 <br/>
-<label for="ID">Age</label>
-<input type="number" name="Age" id="Age">
-<br/>
-<input type="submit" value = "Add Athlete">
-<br/>
+<p> Enter Only USA or China Because i have only two countries in my database</P>
 
-
+<input type="submit" name="search" id="search">
+<br/>
 
 </form>
 
@@ -37,34 +22,18 @@ if($con==true)
 {
  echo "Connection Success<br/> <br/>" . "\xA" . "\xA". "\xA". "\xA";
 }
+$Country = "";
+$Country = isset($_POST['Country']) ? $_POST['Country'] : '';
 
-$Athlete_id = isset($_POST['Athlete_id']) ? $_POST['Athlete_id'] : '';
-$Name = isset($_POST['Name']) ? $_POST['Name'] : '';
-$DOB = isset($_POST['DOB']) ? $_POST['DOB'] : '';
-$Gender= isset($_POST['Gender']) ? $_POST['Gender'] : '';
-$Country= isset($_POST['Country']) ? $_POST['Country'] : '';
-$Age = isset($_POST['Age']) ? $_POST['Age'] : '';
-
-echo isset($_POST['Athlete_id']);
-echo isset($_POST['Name']);
-echo isset($_POST['DOB']);
-echo isset($_POST['Gender']);
-echo isset($_POST['Country']);
-echo isset($_POST['Age']);
-
-
-
-
-
-$query="INSERT INTO athlete ('Athlete_id','Name','DOB','Gender','Country','Age') values ($Athlete_id, $Name,$DOB, $Gender,$Country,$Age)";
-mysqli_query($con,$query) or die (mysql_error());
-
-if(mysqli_affected_rows($con)>1) {
-	echo "<p> Employee Added</p>";
-}
-else{
-	echo "Employee NOT Added <br/>";
-	echo mysqli_error ($con);
+$query="Select * from athlete where Country ='$Country'";
+$fetch = mysqli_query($con,$query) or die (mysql_error());
+while($rows=mysqli_fetch_assoc($fetch)or die (mysql_error())) {
+        //echo  "Athlete_id:    " .$rows['Athlete_id']. "\xA". "\xA". "\xA";
+		echo "Name:    " . $rows['Name']. "\xA". "\xA";
+		//echo  "DOB:   " .$rows ['DOB']. "\xA". "\xA". "\xA";
+		echo  "Gender:   " .$rows['Gender']. "\xA". "\xA". "\xA";
+		echo  "Country:   " .$rows ['Country']. "\xA". "\xA<br/>";
+	   ///echo   "Age:   " . $rows['Age']. "\xA". "\xA<br/>";
 }
 
 ?>
